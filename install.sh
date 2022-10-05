6edb0111b900ae9a1be342beaaffaec701c6a50b
hanool <29415530+hanool@users.noreply.github.com>
2022-10-05 15:38:49 +0900

    :fix install script to target right path

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #! /bin/zsh
 
-ln -sf $(pwd)/tmux/.tmux.conf ~/.tmux.conf
-ln -sfn $(pwd)/nvim ~/.config/nvim
+CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ln -sf "$CURRENT_DIR/tmux/.tmux.conf" "$HOME/.tmux.conf"
+if [ -f ~/.config/nvim ]
+then
+	rm ~/.config/nvim
+fi
+ln -sfn "$CURRENT_DIR/nvim" "$HOME/.config/nvim"
