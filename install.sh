9b3d147d57fa242789b83b90749ee227647d2c62
hanool <29415530+hanool@users.noreply.github.com>
2022-10-14 23:14:44 +0900

    Add gitconfig

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #! /bin/zsh
 
 CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# tmux configs
 ln -sf "$CURRENT_DIR/tmux/.tmux.conf" "$HOME/.tmux.conf"
+
+# git configs
+ln -sf "$CURRENT_DIR/git/.gitconfig" "$HOME/.gitconfig"
+
+# nvim configs
 if [ -f ~/.config/nvim ]
 then
 	rm ~/.config/nvim
