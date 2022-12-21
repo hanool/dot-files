bfb639b3bbb2336533e4a89a68ab40523d858068
hanool <29415530+hanool@users.noreply.github.com>
2022-12-21 23:45:06 +0900

    nvim config: Add Rust Configuration

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -134,6 +134,7 @@ return require('packer').startup(function(use)
   -- LSP Config
   use 'hrsh7th/nvim-cmp' -- Autocompletion plugin
   use 'hrsh7th/cmp-nvim-lsp' -- LSP source for nvim-cmp
+  use 'simrat39/rust-tools.nvim' -- Rust Configuration Plugin
   --use_rocks 'jsregexp'
   use {
     'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
