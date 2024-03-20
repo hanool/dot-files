9a705ab9fb471983f86227a0f0470d88b1b22bc9
hanool <29415530+hanool@users.noreply.github.com>
2024-03-20 17:03:35 +0900

    nvim: update rust lsp setup

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,54 +1,85 @@
 local lsp_zero = require('lsp-zero')
 
 lsp_zero.on_attach(function(client, bufnr)
-	-- see :help lsp-zero-keybindings
-	-- to learn the available actions
-	lsp_zero.default_keymaps({ buffer = bufnr })
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({ buffer = bufnr })
 end)
 
 local cmp = require('cmp')
 
 cmp.setup({
-	mapping = cmp.mapping.preset.insert({
-		['<CR>'] = cmp.mapping.confirm({ select = true }),
-		['<C-Space>'] = cmp.mapping.complete(),
-	})
-})
-
-require('mason').setup({})
-require('mason-lspconfig').setup({
-	ensure_installed = {
-		'tsserver',
-		'eslint',
-		'lua_ls',
-		'rust_analyzer',
-	},
-	handlers = {
-		lsp_zero.default_setup,
-	},
+  mapping = cmp.mapping.preset.insert({
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    ['<C-Space>'] = cmp.mapping.complete(),
+  })
 })
 
 local lsp = require("lspconfig")
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  ensure_installed = {
+    'tsserver',
+    'eslint',
+    'lua_ls',
+    'rust_analyzer',
+  },
+  handlers = {
+    lsp_zero.default_setup,
+    lua_ls = function()
+      lsp.lua_ls.setup {
+        settings = {
+          Lua = {
+            diagnostics = {
+              -- Get the language server to recognize the `vim` global
+              globals = { 'vim' },
+            },
+          },
+        },
+      }
+    end,
+    rust_analyzer = function()
+      lsp.rust_analyzer.setup({
+        on_attach = function(client)
+          require 'completion'.on_attach(client)
+        end,
+        settings = {
+          ["rust-analyzer"] = {
+            imports = {
+              granularity = {
+                group = "module",
+              },
+              prefix = "self",
+            },
+            cargo = {
+              buildScripts = {
+                enable = true,
+              },
+            },
+            procMacro = {
+              enable = true
+            },
+            add_return_type = {
+              enable = true
+            },
+            diagnostics = {
+              enable = true,
+            }
+          }
+        }
+      })
+    end,
+  },
+})
 
-lsp.lua_ls.setup {
-	settings = {
-		Lua = {
-			diagnostics = {
-				-- Get the language server to recognize the `vim` global
-				globals = { 'vim' },
-			},
-		},
-	},
-}
-lsp.volar.setup {}
 
 -- set up LSP signs
 for type, icon in pairs({
-	Error = "",
-	Warn = "",
-	Hint = "",
-	Info = "",
+  Error = "",
+  Warn = "",
+  Hint = "",
+  Info = "",
 }) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
